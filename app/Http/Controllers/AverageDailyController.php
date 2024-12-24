ff2979bb9ea5bacad62968e448643eea156963d4
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-12-24 21:46:25 +0700

    .

--- app/Http/Controllers/AverageDailyController.php
+++ app/Http/Controllers/AverageDailyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AverageDailyRequest;
 use App\Http\Resources\AverageDailyResources;
+
 use App\Http\Resources\GetAverageDailyResources;
 use App\Models\AverageDaily;
 use App\Models\CollectData;
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Log;
 
 class AverageDailyController extends Controller
 {
-    public function GetAverageDaily(AverageDailyRequest $request)
+    public function GetRangeAverageDaily(AverageDailyRequest $request)
     {
         $validated = $request->validated();
         $startDate = $validated['start_date'];
@@ -31,5 +32,32 @@ class AverageDailyController extends Controller
         return GetAverageDailyResources::collection($averageData);
     }
 
-    
+    public function GetAllAverageDaily()
+    {
+        $query = AverageDaily::query();
+        if ($id = request()->query('id')) {
+            $query->where('id', $id);
+        }
+        if ($date = request()->query('date')) {
+            $query->where('date', $date);
+        }
+        if ($avg_temperature = request()->query('avg_temperature')) {
+            $query->where('avg_temperature', $avg_temperature);
+        }
+        if ($avg_air_humidity = request()->query('avg_air_humidity')) {
+            $query->where('avg_air_humidity', $avg_air_humidity);
+        }
+        if ($soil_humidity = request()->query('soil_humidity')) {
+            $query->where('soil_humidity', $soil_humidity);
+        }
+        $data = $query->get();
+        if ($data->isEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data not found',
+            ], 404);
+        }
+
+        return GetAverageDailyResources::collection($data);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,8 @@ use App\Http\Controllers\CollectDataController;
 
 Route::post('/send_collect_data', [CollectDataController::class, 'SendCollectData']);
 Route::get('/get_collect_data', [CollectDataController::class, 'GetAllCollectData']);
-Route::get('/get_average_daily', [AverageDailyController::class, 'GetAverageDaily']);
+Route::get('/get_range_average_daily', [AverageDailyController::class, 'GetRangeAverageDaily']);
+Route::get('/get_all_average_daily', [AverageDailyController::class, 'GetAllAverageDaily']);
 Route::post('/send_message', [ArtificialIntellegenceController::class, 'SendMessage']);
 Route::get('/get_message_data', [ArtificialIntellegenceController::class, 'GetMessageData']);
 Route::get('/get_message_latest', [ArtificialIntellegenceController::class, 'GetLatestMessageData']);
