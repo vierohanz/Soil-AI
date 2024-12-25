0a68eb3df4d367680fc5a698972b2fb9b630752c
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-12-25 18:24:28 +0700

    .

--- app/Console/Commands/MqttListener.php
+++ app/Console/Commands/MqttListener.php
@@ -36,20 +36,18 @@ class MqttListener extends Command
                 $data = json_decode($message, true);
 
                 if ($data) {
+                    // Jika created_at ada, ubah zona waktu ke Asia/Jakarta
                     $createdAt = isset($data['created_at'])
-                        ? Carbon::parse($data['created_at'])->timezone('Asia/Jakarta')
-                        : Carbon::now('Asia/Jakarta');
+                        ? Carbon::parse($data['created_at'])->timezone('Asia/Jakarta')  // Konversi waktu
+                        : Carbon::now('Asia/Jakarta');  // Gunakan waktu saat ini jika tidak ada created_at
 
-                    $updatedAt = isset($data['updated_at'])
-                        ? Carbon::parse($data['updated_at'])->timezone('Asia/Jakarta')
-                        : Carbon::now('Asia/Jakarta');
-
+                    // Menyimpan data ke database dengan zona waktu yang sudah benar
                     CollectData::create([
                         'temperature' => $data['temperature'],
                         'air_humidity' => $data['air_humidity'],
                         'soil_humidity' => $data['soil_humidity'],
-                        'created_at' => $createdAt,
-                        'updated_at' => $updatedAt,
+                        'created_at' => $createdAt, // Waktu dengan zona waktu Asia/Jakarta
+                        'updated_at' => $createdAt, // Waktu dengan zona waktu Asia/Jakarta
                     ]);
                     echo "Data saved to database: " . json_encode($data) . PHP_EOL;
                 } else {
